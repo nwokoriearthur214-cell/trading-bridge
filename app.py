f1b5b96a39f683a8e76b5274068d40945c4f6129
nwokoriearthur214-cell <nwokoriearthur214@gmail.com>
2026-06-02 16:33:14 -0500

    app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def hello():
     return "Bridge is active"
 
 if __name__ == '__main__':
-    # This specifically tells Render to use the port it provided
+    # This specifically tells the server to stay open
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
